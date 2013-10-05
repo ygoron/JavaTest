b2eb1d29ee9c59361fca27518aaffa61074cc74b
ygoron <ygoron@gmail.com>
2013-10-05 13:16:58 -0400

    22

--- TestUtility/src/main/java/com/apos/test/ExampleService.java
+++ TestUtility/src/main/java/com/apos/test/ExampleService.java
@@ -13,7 +13,7 @@ public class ExampleService implements Service {
 	 * Reads next record from input
 	 */
 	public String getMessage() {
-		return "Hello world! With Git 2";	
+		return "Hello world! With Git 22";	
 	}
 
 }
